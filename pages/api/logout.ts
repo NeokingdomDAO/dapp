37d6e1f5b8596fb92aa5278cfb49893d2d98aba0
Gianluca Donato <gian.dnt@gmail.com>
2023-01-20 14:37:27 +0100

    add userFactory

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,11 +1,11 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 import { sessionOptions } from "../../lib/session";
 import { NextApiRequest, NextApiResponse } from "next";
-import type { User } from "./user";
+import userFactory, { User } from "../../lib/userFactory";
 
 function logoutRoute(req: NextApiRequest, res: NextApiResponse<User>) {
   req.session.destroy();
-  res.json({ uid: -1, username: "", password: "", isLoggedIn: false });
+  res.json(userFactory());
 }
 
 export default withIronSessionApiRoute(logoutRoute, sessionOptions);
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,17 +1,11 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 import { sessionOptions } from "../../lib/session";
 import { NextApiRequest, NextApiResponse } from "next";
-
-export type User = {
-  uid: number;
-  username: string;
-  password: string;
-  isLoggedIn: boolean;
-};
+import userFactory, { User } from "../../lib/userFactory";
 
 async function userRoute(req: NextApiRequest, res: NextApiResponse<User>) {
   if (!req.session.user) {
-    return res.json({ uid: -1, username: "", password: "", isLoggedIn: false });
+    return res.json(userFactory());
   }
   res.json({
     ...req.session.user,
