4629c943c0a4c23c460cf98af52cee6bc52268ca
Gianluca Donato <gian.dnt@gmail.com>
2023-03-10 03:02:06 +0100

    bring pwa back

--- next.config.js
+++ next.config.js
@@ -3,21 +3,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
 });
 
-// module.exports = withPWA({
-//   reactStrictMode: false,
-//   compiler: {
-//     emotion: true,
-//   },
-//   typescript: {
-//     // !! WARN !!
-//     // Dangerously allow production builds to successfully complete even if
-//     // your project has type errors.
-//     // !! WARN !!
-//     ignoreBuildErrors: true,
-//   },
-// });
-
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: false,
   compiler: {
     emotion: true,
@@ -29,4 +15,4 @@ module.exports = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-};
+});
