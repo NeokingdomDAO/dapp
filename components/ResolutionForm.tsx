8e8a1dbd4f605096fbea21c44fc33b51e35e5450
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2023-07-21 14:35:25 +0200

    feat: wip exclusion

--- components/ResolutionForm.tsx
+++ components/ResolutionForm.tsx
@@ -1,6 +1,6 @@
 import "easymde/dist/easymde.min.css";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import {
   Alert,
@@ -9,8 +9,10 @@ import {
   FormControl,
   FormControlLabel,
   FormLabel,
+  Paper,
   Radio,
   RadioGroup,
+  Switch,
   TextField,
 } from "@mui/material";
 import { Grid, Typography } from "@mui/material";
@@ -20,15 +22,19 @@ import { getPreviousMonth } from "@lib/resolutions/common";
 import useResolutionTypes from "@hooks/useResolutionTypes";
 
 import { RESOLUTION_TYPES_TEXTS } from "../i18n/resolution";
+import UsersAutocomplete from "./UsersAutocomplete";
 
 interface FormProps {
   isMonthlyRewards?: boolean;
   title: string;
   content: string;
   typeId: string;
+  exclusionAddress: string;
   onUpdateTitle: (evt: any) => void;
   onUpdateType: (evt: any) => void;
   onUpdateContent: (content: string) => void;
+  onUpdateExclusionAddress: (address: string) => void;
+  isEditing?: boolean;
 }
 
 export default function ResolutionForm({
@@ -36,12 +42,17 @@ export default function ResolutionForm({
   title,
   content,
   typeId,
+  exclusionAddress,
   onUpdateTitle,
   onUpdateType,
   onUpdateContent,
+  onUpdateExclusionAddress,
+  isEditing = false,
 }: FormProps) {
   const { types, isLoading: isLoadingTypes } = useResolutionTypes();
   const editorRef = useRef(null);
+
+  const [withExclusion, setWithExclusion] = useState(exclusionAddress !== "");
 
   useEffect(() => {
     let easyMdeInstance: any = null;
@@ -138,6 +149,27 @@ export default function ResolutionForm({
               </RadioGroup>
             )}
           </FormControl>
+        </Grid>
+        {/* TODO - if isEditing make this readonly (and get addressedContributor from graph) */}
+        <Grid item xs={12} sx={{ mt: 4 }}>
+          <Paper sx={{ p: 4 }}>
+            <FormControlLabel
+              control={<Switch checked={withExclusion} onChange={() => setWithExclusion((old) => !old)} />}
+              label="Resolution with exclusion"
+            />
+            {withExclusion && (
+              <Box sx={{ mt: 4 }}>
+                <UsersAutocomplete
+                  exclusionAddress={exclusionAddress}
+                  onChange={(address) => onUpdateExclusionAddress(address)}
+                />
+              </Box>
+            )}
+            <Alert severity="info" sx={{ mt: 4 }}>
+              You can decide to exclude one contributor from the resolution. This contributor will not be able to vote
+              on such resolution and their vote will not count.
+            </Alert>
+          </Paper>
         </Grid>
       </Grid>
     </div>
