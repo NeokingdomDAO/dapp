41c1138c463824d5473b4f48c1e1111e8688e588
Gianluca Donato <gian.dnt@gmail.com>
2023-08-21 21:12:20 +0200

    convert walletClient to signer

--- hooks/useContracts.ts
+++ hooks/useContracts.ts
@@ -1,3 +1,4 @@
+import { Signer, providers } from "ethers";
 import { SUPPORTED_CHAINS } from "pages/_app";
 import { WalletClient, useAccount, useDisconnect, useNetwork, useWalletClient } from "wagmi";
 
@@ -26,37 +27,37 @@ import networksTeledisko from "../networks/teledisko.json";
 const networks: Record<string, any> =
   process.env.NEXT_PUBLIC_PROJECT_KEY === "neokingdom" ? networksNeoKingdom : networksTeledisko;
 
-const getResolutionManagerContract = (chainId: string, walletClient: WalletClient): ResolutionManager => {
+const getResolutionManagerContract = (chainId: string, signer: Signer): ResolutionManager => {
   const address = networks[chainId]["ResolutionManager"]?.address;
-  return ResolutionManager__factory.connect(address, walletClient);
+  return ResolutionManager__factory.connect(address, signer);
 };
 
-const getNeokingdomTokenContract = (chainId: string, walletClient: WalletClient): NeokingdomToken => {
+const getNeokingdomTokenContract = (chainId: string, signer: Signer): NeokingdomToken => {
   const address = networks[chainId]["NeokingdomToken"]?.address;
-  return NeokingdomToken__factory.connect(address, walletClient);
+  return NeokingdomToken__factory.connect(address, signer);
 };
 
 export const getNeokingdomTokenContractAddress = (chainId: string): string => {
   return networks[chainId]["NeokingdomToken"]?.address;
 };
 
-const getVotingContract = (chainId: string, walletClient: WalletClient): Voting => {
+const getVotingContract = (chainId: string, signer: Signer): Voting => {
   const address = networks[chainId]["Voting"]?.address;
-  return Voting__factory.connect(address, walletClient);
+  return Voting__factory.connect(address, signer);
 };
 
-const getInternalMarketContract = (chainId: string, walletClient: WalletClient): InternalMarket => {
+const getInternalMarketContract = (chainId: string, signer: Signer): InternalMarket => {
   const address = networks[chainId]["InternalMarket"]?.address;
-  return InternalMarket__factory.connect(address, walletClient);
+  return InternalMarket__factory.connect(address, signer);
 };
 
 const getInternalMarketContractAddress = (chainId: string): string => {
   return networks[chainId]["InternalMarket"]?.address;
 };
 
-const getGovernanceTokenContract = (chainId: string, walletClient: WalletClient): GovernanceToken => {
+const getGovernanceTokenContract = (chainId: string, signer: Signer): GovernanceToken => {
   const address = networks[chainId]["GovernanceToken"]?.address;
-  return GovernanceToken__factory.connect(address, walletClient);
+  return GovernanceToken__factory.connect(address, signer);
 };
 
 const getGovernanceTokenContractAddress = (chainId: string): string => {
@@ -64,10 +65,22 @@ const getGovernanceTokenContractAddress = (chainId: string): string => {
 };
 
 // todo use ERC20?
-const getUsdcContract = (chainId: string, walletClient: WalletClient): TokenMock => {
+const getUsdcContract = (chainId: string, signer: Signer): TokenMock => {
   const address = networks[chainId]["TokenMock"]?.address;
-  return TokenMock__factory.connect(address, walletClient);
+  return TokenMock__factory.connect(address, signer);
 };
+
+function walletClientToSigner(walletClient: WalletClient) {
+  const { account, chain, transport } = walletClient;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
+  const provider = new providers.Web3Provider(transport, network);
+  const signer = provider.getSigner(account.address);
+  return signer;
+}
 
 export function useContracts() {
   const { address } = useAccount();
@@ -82,20 +95,21 @@ export function useContracts() {
     if (address && walletClient) {
       try {
         const chainId = String(chain?.id);
+        const signer = walletClientToSigner(walletClient);
 
         if (!SUPPORTED_CHAINS.map((chain) => String(chain.id)).includes(chainId)) {
           throw new Error(`You're connected to an unsupported network, please connect to ${SUPPORTED_CHAINS[0].name}`);
         }
 
         setContracts({
-          resolutionManagerContract: getResolutionManagerContract(chainId, walletClient),
-          neokingdomTokenContract: getNeokingdomTokenContract(chainId, walletClient),
-          votingContract: getVotingContract(chainId, walletClient),
-          internalMarketContract: getInternalMarketContract(chainId, walletClient),
+          resolutionManagerContract: getResolutionManagerContract(chainId, signer),
+          neokingdomTokenContract: getNeokingdomTokenContract(chainId, signer),
+          votingContract: getVotingContract(chainId, signer),
+          internalMarketContract: getInternalMarketContract(chainId, signer),
           internalMarketContractAddress: getInternalMarketContractAddress(chainId),
-          governanceTokenContract: getGovernanceTokenContract(chainId, walletClient),
+          governanceTokenContract: getGovernanceTokenContract(chainId, signer),
           governanceTokenContractAddress: getGovernanceTokenContractAddress(chainId),
-          usdcContract: getUsdcContract(chainId, walletClient),
+          usdcContract: getUsdcContract(chainId, signer),
         });
       } catch (error) {
         console.error(error);
