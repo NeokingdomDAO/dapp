4c019d7b306c9e00f20958808b8e4a2194843e08
Alberto Granzotto <alberto@granzotto.net>
2024-05-05 19:27:11 +0200

    Use keccak256, sort keys

--- pages/api/resolutions/new.ts
+++ pages/api/resolutions/new.ts
@@ -1,24 +1,11 @@
 import { addResolution, getResolution } from "drizzle/db";
+import { keccak256, toUtf8Bytes } from "ethers/lib/utils";
 import { withIronSessionApiRoute } from "iron-session/next";
+import stringifyDeterministic from "json-stringify-deterministic";
 import { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 
 import { sessionOptions } from "@lib/session";
-
-async function sha256(message: string) {
-  // encode as UTF-8
-  const msgBuffer = new TextEncoder().encode(message);
-
-  // hash the message
-  const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
-
-  // convert ArrayBuffer to Array
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-
-  // convert bytes to hex string
-  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
-  return hashHex;
-}
 
 const ResolutionData = z.object({
   title: z.string().min(1),
@@ -40,7 +27,7 @@ async function createNewResolution(req: NextApiRequest, res: NextApiResponse) {
   }
 
   try {
-    const hash = await sha256(JSON.stringify(result.data));
+    const hash = keccak256(toUtf8Bytes(stringifyDeterministic(result.data)));
 
     const existingResolution = await getResolution(hash);
 
