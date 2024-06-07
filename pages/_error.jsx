3b658df269b5fe3fa80aee9b2279f3ea4cb71f23
Andrea Tosatto <andrea.tosatto@infinitaslearning.com>
2024-06-07 23:15:47 +0300

    improve sentry config

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -9,6 +9,7 @@ CustomErrorComponent.getInitialProps = async (contextData) => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
   await Sentry.captureUnderscoreErrorException(contextData);
+  console.error("Error Boundary", contextData);
 
   // This will contain the status code of the response
   return Error.getInitialProps(contextData);
